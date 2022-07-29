4308f2ddf7713f3162812a984aa0f76bedf900d3
Geoff Maddock <geoff.maddock@gmail.com>
2022-07-29 03:02:21 -0400

    Added the project card component.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    import ProjectCard from '$lib/components/project-card.svelte'
+    import { client } from '$lib/graphql-client'
+    import { gql } from 'graphql-request'
+  
+    export const load = async () => {
+  
+      const query = gql`
+        query GetProjects {
+          projects {
+            name
+            slug
+            description
+            demo
+            sourceCode
+            image {
+              url
+            }
+          }
+        }
+      `
+  
+      const { projects } = await client.request(query)
+  
+      return {
+        props: {
+          projects,
+        },
+      }
+    }
+  </script>
+
+<script>
+    export let projects
+  </script>
+  
+  <h1>Portfolio Projects</h1>
+  {#each projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} url={image[0].url} {slug}/>
+  {/each}
